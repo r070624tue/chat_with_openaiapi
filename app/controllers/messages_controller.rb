9be8789a601d08e5d84f76f6d62414dbd95da469
r070624tue <h290404tue@gmail.com>
2025-08-24 18:51:31 +0900

    サーバーサイドのエラーハンドリング

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -9,10 +9,18 @@ class MessagesController < ApplicationController
   def create
     @message = Message.new(message_params)
     response = openai_api_call(@message.prompt)
-    response_body = JSON.parse(response.body)
-    @message.response = response_body['choices'][0]['message']['content']
-    @message.save
-    render json: { response: @message.response }
+
+    if response.status.success?
+      response_body = JSON.parse(response.body)
+      @message.response = response_body['choices'][0]['message']['content']
+      if @message.save
+        render json: { response: @message.response }
+      else
+        render json: { error: @message.errors.full_messages.join(', ') }, status: :unprocessable_entity
+      end
+    else
+      render json: { error: 'APIリクエストが失敗しました' }, status: :unprocessable_entity
+    end
   end
 
   private
