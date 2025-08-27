33a2f3c4eefbcef0a67706ed265a47f781eba4b4
r070624tue <h290404tue@gmail.com>
2025-08-27 16:07:30 +0900

    取得したスレッドをクライアントサイドに返す

--- app/controllers/chat_threads_controller.rb
+++ app/controllers/chat_threads_controller.rb
@@ -7,4 +7,13 @@ class ChatThreadsController < ApplicationController
   def create
     @chat_thread = ChatThread.create(title: 'Untitled')
   end
+
+  def show
+    @chat_thread = ChatThread.find(params[:id])
+
+    respond_to do |format|
+      format.json { render json: { chat_thread: @chat_thread } }
+      format.html { render 'messages/index' }
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root "messages#index"
-  resources :chat_threads, only: [:index, :create] do
+  resources :chat_threads, only: [:index, :create, :show] do
     resources :messages, only: [:index, :create]
   end
 end
