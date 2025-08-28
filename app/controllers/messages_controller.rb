6e5d30ee4b76382c78ae7b30faf7a2c555ca9509
r070624tue <h290404tue@gmail.com>
2025-08-28 20:53:49 +0900

    ビューを修正

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,11 +1,6 @@
 require 'http'
 
 class MessagesController < ApplicationController
-  def index
-    @messages = Message.all
-    @message = Message.new
-  end
-
   def create
     @message = Message.new(message_params)
     response = openai_api_call(@message.prompt)
